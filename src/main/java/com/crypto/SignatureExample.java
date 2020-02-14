967b83005a3bd8d11c2fa53cd656c5b791a69b0e
shubo zhang <szhang@tenable.com>
2020-02-14 07:26:18 -0500

    added  comments to signature example

--- src/main/java/com/crypto/SignatureExample.java
+++ src/main/java/com/crypto/SignatureExample.java
@@ -11,24 +11,27 @@ public class SignatureExample {
     }
 
     private static void signatureExampleTest() throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        // 1. Certificate Authority generate their public/private key pair
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
         keyPairGenerator.initialize(1024);
         KeyPair keyPair = keyPairGenerator.generateKeyPair();
         printByteArray("PrivateKey", keyPair.getPrivate().getEncoded());
         printByteArray("PublicKey", keyPair.getPublic().getEncoded());
 
-        // input
+        // 2-1. A digest message example of certificate from a company (for example: aol.com)
         byte[] input = "The Lord of the Rings has been read by many people".getBytes();
         printText("input", input);
 
-        // Signature: both hashing and encryption
+        // 2-2. CA uses their private key to encrypt the certificate and then generate signature. This signature is attached with
+        // certificate. Signature: both hashing and encryption
         Signature signature = Signature.getInstance("SHA256WithRSA");
         signature.initSign(keyPair.getPrivate());
         signature.update(input);
         byte[] signedSignature = signature.sign();
         printByteArray("Signed Signature", signedSignature);
 
-        // Verification on the other end
+        // 3. Client can use CA's open public key to verify the signature.
+        // For example: ca_publickey.decrypt(signature) should match certificate digest message.
         Signature verifySignature = Signature.getInstance("SHA256WithRSA");
         verifySignature.initVerify(keyPair.getPublic());
         verifySignature.update(input);
